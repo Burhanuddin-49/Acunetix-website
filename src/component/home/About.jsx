37fe0b93c16ff6cdaaa4012caf008d6800b9d849
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-10-18 01:13:06 +0530

    Animation added

--- src/component/home/About.jsx
+++ src/component/home/About.jsx
@@ -1,7 +1,24 @@
 import React from "react";
 import "./styles/About.css";
+import img1 from "../../image/A1.png";
+import img2 from "../../image/A2.png";
+import img3 from "../../image/A3.png";
+import img4 from "../../image/A4.png";
+import img5 from "../../image/A5.png";
+import img6 from "../../image/A6.png";
+import img7 from "../../image/A7.png";
 
 const About = () => {
+  document.addEventListener("mousemove", parallax);
+  function parallax(e){
+    document.querySelectorAll(".object").forEach(function (move) {
+      var moving_value = move.getAttribute("datavalue");
+      var x = (e.clientX * moving_value)/250;
+      var y = (e.clientY * moving_value)/250;
+
+      move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+    });
+  };
   return (
     <div className="about">
       <div className="split left">
@@ -15,15 +32,14 @@ const About = () => {
           sapiente, veniam ad nostrum incidunt, sunt possimus nam? Libero, at.
         </p>
       </div>
-      <div className="split right">
-        <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aut porro
-          eum quos, cupiditate ratione eaque sapiente, officia ab dolorum
-          dolores nobis odit laborum nemo. Hic alias facere maiores odio
-          corrupti consectetur enim fugiat sequi esse, ex sapiente? Non illo
-          dolore quibusdam quae quasi, nemo fuga vero qui itaque, numquam
-          magnam.
-        </p>
+      <div className="split right container1">
+        <img src={img1} className="object" datavalue="-2" alt="" />
+        <img src={img2} className="object" datavalue="2" alt="" />
+        <img src={img3} className="object" datavalue="5" alt="" />
+        <img src={img4} className="object" datavalue="-5" alt="" />
+        <img src={img5} className="object" datavalue="4" alt="" />
+        <img src={img6} className="object" datavalue="6" alt="" />
+        <img src={img7} className="object" datavalue="3" alt="" />
       </div>
     </div>
   );
