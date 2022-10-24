b6ebd5ecc9c811faeda676366c76a3f1f422516b
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-10-24 03:08:32 +0300

    sponsor catalog added

--- src/component/event/Card.js
+++ src/component/event/Card.js
@@ -1,44 +1,87 @@
 import React from "react";
 import "./Card.css";
-import event from '../../image/event.jpg'
-
+import event from "../../image/event.jpg";
 
 export default function cards2() {
-const data = [
-  {id: 1, name: "game", img: event, date: "2 Dec"},
-  {id: 2, name: "game", img: event, date: "2 Dec"},
-  {id: 3, name: "game", img: event, date: "2 Dec"},
-  {id: 3, name: "game", img: event, date: "2 Dec"},
-  {id: 3, name: "game", img: event, date: "2 Dec"},
-  {id: 3, name: "game", img: event, date: "2 Dec"},
-  {id: 3, name: "game", img: event, date: "2 Dec"},
-]
+  const data = [
+    { id: 1, name: "game", img: event, date: "2 Dec" },
+    { id: 2, name: "game", img: event, date: "2 Dec" },
+    { id: 3, name: "game", img: event, date: "2 Dec" },
+    { id: 3, name: "game", img: event, date: "2 Dec" },
+    { id: 3, name: "game", img: event, date: "2 Dec" },
+    { id: 3, name: "game", img: event, date: "2 Dec" },
+    { id: 3, name: "game", img: event, date: "2 Dec" },
+  ];
 
   return (
-    <div className="bodyEvent">
-      <div className="containerEvent">
-        <div className="headingEvent">
-          <h2>Here are all the </h2>
-          <h2>Amazing Events </h2>
-        </div>
-
-        {data.map((data) => (
-          <div className="cardEvent">
-            <div className="box">
-              <div className="content">
-                <img src={data.img} alt="" />
-                <h4>{data.date}</h4>
-                <a href="/event" className="more">
-                  Read more
-                </a>
-                <a href="/event" className="register">
-                  Register
-                </a>
-              </div>
+    <>
+      <div className="container">
+        <div
+          id="carouselExampleInterval"
+          className="carousel slide"
+          data-bs-ride="carousel"
+        >
+          <div className="carousel-inner">
+            <div className="carousel-item active" data-bs-interval="3000">
+              <img src={event} className="d-block w-100" alt="" />
+            </div>
+            <div className="carousel-item" data-bs-interval="3000">
+              <img src={event} className="d-block w-100" alt="" />
+            </div>
+            <div className="carousel-item" data-bs-interval="3000">
+              <img src={event} className="d-block w-100" alt="" />
             </div>
           </div>
-        ))}
+          <button
+            className="carousel-control-prev"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="prev"
+          >
+            <span
+              className="carousel-control-prev-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Previous</span>
+          </button>
+          <button
+            className="carousel-control-next"
+            type="button"
+            data-bs-target="#carouselExampleInterval"
+            data-bs-slide="next"
+          >
+            <span
+              className="carousel-control-next-icon"
+              aria-hidden="true"
+            ></span>
+            <span className="visually-hidden">Next</span>
+          </button>
+        </div>
       </div>
-    </div>
+      <div className="bodyEvent">
+        <div className="containerEvent">
+          <div className="headingEvent">
+            <h2>Here are all the Amazing Events</h2>
+          </div>
+
+          {data.map((data) => (
+            <div className="cardEvent">
+              <div className="box">
+                <div className="content">
+                  <img src={data.img} alt="" />
+                  <h4>{data.date}</h4>
+                  <a href="/event" className="more">
+                    Read more
+                  </a>
+                  <a href="/event" className="register">
+                    Register
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
--- src/component/home/Home.jsx
+++ src/component/home/Home.jsx
@@ -1,19 +1,21 @@
 import React from "react";
 import "./styles/Home.css";
 // import video from "../../video/mv1.mp4";
-import About from "./About"
+import About from "./About";
 import Footer from "./Footer";
 import Title from "./Title";
 import EventCatalog from "./EventCatalog";
+import SponsorCatalog from "./SponsorCatalog";
 const Home = () => {
   return (
     <div className="body">
       {/* <video src={video} autoPlay muted loop className="home-bg-video" />  */}
       <div className="section">
-          <Title />
-          <About />
-          <EventCatalog/>
-          <Footer />
+        <Title />
+        <About />
+        <EventCatalog />
+        <SponsorCatalog />
+        <Footer />
       </div>
     </div>
   );
