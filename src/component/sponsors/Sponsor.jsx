1550f9a4182e0c16e7aa53b6e2715ecb2af8c7e6
Burhanuddin-49 <burhanuddin9651@gmail.com>
2022-10-26 02:19:46 +0300

    sponsor updated

--- src/component/sponsors/Sponsor.jsx
+++ src/component/sponsors/Sponsor.jsx
@@ -4,157 +4,151 @@ import sponsorImg from "../../image/sponsor.jpg";
 
 const Sponsor = () => {
   return (
-    <>
+    <div className="sponsor">
       <div className="sponsor-title">
         <h1>Sponsors</h1>
       </div>
-      <div className="sponsor">
-        <div className="card_collection">
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src={sponsorImg}
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt eius ad maiores, soluta cumque omnis aspernatur ea
-                totam, quibusdam dolore accusantium ratione neque! Dignissimos
-                doloribus esse blanditiis, pariatur deleniti similique!
-              </div>
+      <div className="card_collection">
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src={sponsorImg}
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              eius ad maiores, soluta cumque omnis aspernatur ea totam,
+              quibusdam dolore accusantium ratione neque! Dignissimos doloribus
+              esse blanditiis, pariatur deleniti similique!
             </div>
           </div>
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src={sponsorImg}
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates omnis error reiciendis magni ipsum, nisi
-                exercitationem quibusdam enim. Consequatur similique laborum
-                quos aut mollitia accusamus voluptatem obcaecati eum
-                reprehenderit!
-              </div>
+        </div>
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src={sponsorImg}
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates omnis error reiciendis magni ipsum, nisi exercitationem
+              quibusdam enim. Consequatur similique laborum quos aut mollitia
+              accusamus voluptatem obcaecati eum reprehenderit!
             </div>
           </div>
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src="spon_5.png"
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Deserunt eius ad maiores, soluta cumque omnis aspernatur ea
-                totam, quibusdam dolore accusantium ratione neque! Dignissimos
-                doloribus esse blanditiis, pariatur deleniti similique!
-              </div>
+        </div>
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src="spon_5.png"
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt
+              eius ad maiores, soluta cumque omnis aspernatur ea totam,
+              quibusdam dolore accusantium ratione neque! Dignissimos doloribus
+              esse blanditiis, pariatur deleniti similique!
             </div>
           </div>
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src="spon_3.png"
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates omnis error reiciendis magni ipsum, nisi
-                exercitationem quibusdam enim. Consequatur similique laborum
-                quos aut mollitia accusamus voluptatem obcaecati eum
-                reprehenderit!
-              </div>
+        </div>
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src="spon_3.png"
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates omnis error reiciendis magni ipsum, nisi exercitationem
+              quibusdam enim. Consequatur similique laborum quos aut mollitia
+              accusamus voluptatem obcaecati eum reprehenderit!
             </div>
           </div>
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src="spon_4.png"
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates omnis error reiciendis magni ipsum, nisi
-                exercitationem quibusdam enim. Consequatur similique laborum
-                quos aut mollitia accusamus voluptatem obcaecati eum
-                reprehenderit!
-              </div>
+        </div>
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src="spon_4.png"
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates omnis error reiciendis magni ipsum, nisi exercitationem
+              quibusdam enim. Consequatur similique laborum quos aut mollitia
+              accusamus voluptatem obcaecati eum reprehenderit!
             </div>
           </div>
-          <div className="service-block-two">
-            <div className="inner-box card">
-              <div className="spon_type">Sponsorship Type</div>
-              <div className="shape-one"></div>
-              <div className="shape-two"></div>
-              <div>
-                <a href="/">
-                  <div className="spon_name">Sponsor Name</div>
-                  <img
-                    src="spon_5.png"
-                    className="spon_img"
-                    alt="Sponsors Logo"
-                  />
-                </a>
-              </div>
-              <div className="text">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                Voluptates omnis error reiciendis magni ipsum, nisi
-                exercitationem quibusdam enim. Consequatur similique laborum
-                quos aut mollitia accusamus voluptatem obcaecati eum
-                reprehenderit!
-              </div>
+        </div>
+        <div className="service-block-two">
+          <div className="inner-box card">
+            <div className="spon_type">Sponsorship Type</div>
+            <div className="shape-one"></div>
+            <div className="shape-two"></div>
+            <div>
+              <a href="/">
+                <div className="spon_name">Sponsor Name</div>
+                <img
+                  src="spon_5.png"
+                  className="spon_img"
+                  alt="Sponsors Logo"
+                />
+              </a>
+            </div>
+            <div className="text">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptates omnis error reiciendis magni ipsum, nisi exercitationem
+              quibusdam enim. Consequatur similique laborum quos aut mollitia
+              accusamus voluptatem obcaecati eum reprehenderit!
             </div>
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
